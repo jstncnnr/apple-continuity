5a195f4753e6104bed0cf8a42e5ba94567476240
Justin Conner <jstncnnr@outlook.com>
2024-03-23 00:24:28 -0700

    Decode buffer into Proximity Pairing message

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,45 @@
+use std::fmt::Display;
+
 pub mod messages;
 
+#[derive(Debug)]
+pub enum ErrorKind {
+    DecodeError,
+}
+
+#[derive(Debug)]
+pub struct Error {
+    pub kind: ErrorKind,
+    pub message: Option<String>,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, message: &str) -> Error {
+        Error {
+            kind,
+            message: Some(message.to_string()),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {}",
+            self.kind,
+            self.message.as_deref().unwrap_or("")
+        )
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(value: ErrorKind) -> Self {
+        Error {
+            kind: value,
+            message: None,
+        }
+    }
+}
--- src/messages.rs
+++ src/messages.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::{Error, ErrorKind};
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum MessageType {
     Airprint = 0x03,
@@ -34,4 +36,53 @@ pub struct ProximityPairMessage {
     pub lid_open_count: u8,
     pub device_color: u8,
     pub encrypted_payload: Vec<u8>,
+}
+
+impl ProximityPairMessage {
+    pub fn decode(data: &[u8]) -> Result<ProximityPairMessage, Error> {
+        if data.len() < 2 {
+            return Err(Error::new(
+                ErrorKind::DecodeError,
+                "Length mismatch. Cannot read opcode + length",
+            ));
+        }
+
+        let opcode = data[0];
+        if opcode != 0x07 {
+            return Err(Error::new(
+                ErrorKind::DecodeError,
+                "Tried to decode Proximity Pair message with invalid opcode. Expected 0x07",
+            ));
+        }
+
+        let length = data[1] as usize;
+        if data.len() < length {
+            return Err(Error::new(
+                ErrorKind::DecodeError,
+                "Packet length != buffer length",
+            ));
+        }
+
+        let mut encrypted_payload = vec![0; length - 11];
+        encrypted_payload.copy_from_slice(&data[11..length]);
+
+        Ok(ProximityPairMessage {
+            header: MessageHeader { opcode, length },
+            device_model: ((data[3] as u16) << 8) + (data[4] as u16),
+            device_status: data[5],
+            battery1: data[6],
+            battery2: data[7],
+            lid_open_count: data[8],
+            device_color: data[9],
+            encrypted_payload,
+        })
+    }
+}
+
+impl TryFrom<&[u8]> for ProximityPairMessage {
+    type Error = crate::Error;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Self::decode(value)
+    }
 }
